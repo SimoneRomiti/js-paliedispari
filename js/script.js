66b2a6b27c0a40730e42a939d5f1e8b78c2e6da2
Simone Romiti <sr1992ei@gmail.com>
2020-11-18 15:08:56 +0100

    Commit iniziale

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Chiedere all’utente di inserire una parola.
+// Creare una funzione per capire se la parola inserita è palindroma.
+
+// function palindroma(parola){
+//   for(var i = 0; i < parola.length / 2; i++){
+//     for(var j = parola.length; i > = 0; i--){
+//
+//     }
+//   }
+// }
+alert("fjajfa");
+var sceltaUtente = prompt("Inserisci una parola");
+console.log(sceltaUtente.length / 2);
